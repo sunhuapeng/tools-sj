576ac8d5f842e7ae6fd61997c71bd310714476d6
孙华鹏 <sunhuapeng@heydayscape.com>
2021-02-22 16:34:27 +0800

    调试工具

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,6 +1,7 @@
-const fun = function () {
-  console.log('test')
+const tools =  {
+  trim(str: string, type: boolean): string {
+    console.log('test')
+		return type ? str.replace(/\s+/g, '') : str.replace(/^\s+|\s+$/g, '');
+	},
 }
-export {
-  fun
-}
+export default tools;
