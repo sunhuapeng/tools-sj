0e10c007bec076aade56696646ff3f4214bda63c
孙华鹏 <sunhuapeng@heydayscape.com>
2021-02-23 09:26:53 +0800

    调试

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,6 +1,8 @@
-export default {
+class Tools {
+  constructor(){}
   trim(str: string, type: boolean): string {
     console.log('test')
 		return type ? str.replace(/\s+/g, '') : str.replace(/^\s+|\s+$/g, '');
-	},
+	}
 }
+export default Tools
