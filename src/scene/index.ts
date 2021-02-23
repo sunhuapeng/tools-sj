89ce905b802afcbc54074321a83b942ab582d860
孙华鹏 <sunhuapeng@heydayscape.com>
2021-02-23 14:05:02 +0800

    调试

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,7 +1,1 @@
-// function trim(str: string, type: boolean): string {
-//   return type ? str.replace(/\s+/g, '') : str.replace(/^\s+|\s+$/g, '');
-// }
-// export {
-//   trim
-// }
 module.exports = require('./tools')
--- src/scene/tools.ts
+++ src/scene/tools.ts
@@ -1,4 +1,9 @@
 import trim from './string'
+function test(str: string): string {
+  return str + '-test'
+} 
+
 export {
-  trim
+  trim,
+  test
 }
