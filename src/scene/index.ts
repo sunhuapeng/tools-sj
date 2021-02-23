7205966f3c114df42147182ca8192a69d21f9d8a
孙华鹏 <sunhuapeng@heydayscape.com>
2021-02-23 10:20:03 +0800

    调试

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,10 +1,8 @@
-class Tools {
-  constructor(){}
+const tools = {
   trim(str: string, type: boolean): string {
-    console.log('test')
 		return type ? str.replace(/\s+/g, '') : str.replace(/^\s+|\s+$/g, '');
 	}
 }
 export {
-  Tools
+  tools
 }
